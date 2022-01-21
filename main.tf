d56f74acc5c207c121df6e93d5d84db2689ff8b9
josetakeru <23168163+josetakeru@users.noreply.github.com>
2022-01-21 14:51:45 +0100

    Add second object test

--- main.tf
+++ main.tf
@@ -18,3 +18,16 @@ resource "local_file" "tierObjectFile" {
     content     = "I am a test object file"
     filename = "${path.module}/tierObject.txt"
 }
+
+resource "aws_s3_bucket_object" "tierObject2" {
+    bucket = aws_s3_bucket.tierbucket-cicd-test.id
+    key    = "tierObject2"
+    source = "${path.module}/tierObject2.txt"
+
+    depends_on = [local_file.tierObjectFile2]
+}
+
+resource "local_file" "tierObjectFile2" {
+    content     = "I am a second test object file"
+    filename = "${path.module}/tierObject2.txt"
+}
