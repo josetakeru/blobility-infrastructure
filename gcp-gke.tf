b3841e9815e1023271c791ce1a38190a24ba216b
josetakeru <23168163+josetakeru@users.noreply.github.com>
2022-01-21 21:08:41 +0100

    Add gcp-gke.tf

---
+++ gcp-gke.tf
@@ -0,0 +1,12 @@
+provider "google" {
+    credentials = file()
+    project     = "tier-blobility"
+    region      = "europe-west3"
+    zone        = "europe-west3-a"
+}
+
+resource "google_container_cluster" "kubernetes-cluster" {
+  name               = "tier-blobility-gke-cluster"
+  location           = "europe-west3-a"
+  initial_node_count = 1
+}
